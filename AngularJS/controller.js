892bdff3cbf3b725cfb64d6a855a07603564a092
matthew1996i <matthew199624@gmail.com>
2021-02-02 13:27:24 -0300

    Lesson about routing using angular routing

---
+++ AngularJS/controller.js
@@ -0,0 +1,14 @@
+let app = angular.module("mainApp", ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when("/", {
+      template: "Welcome user!",
+    })
+    .when("/anotherPage", {
+      template: "Welcome user, again!",
+    })
+    .otherwise({
+      redirectTo: "/",
+    });
+});
