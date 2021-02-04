85dc2ae3869cd9698e5958c2bdb7888975537b30
matthew1996i <matthew199624@gmail.com>
2021-02-04 12:23:26 -0300

    save tasks on the local storage and styled project

--- AngularJS/To-do list/controller5.js
+++ AngularJS/To-do list/controller5.js
@@ -3,6 +3,12 @@ let app = angular.module("mainApp", []);
 app.controller("app", ($scope) => {
   $scope.tasks = [];
 
+  let taskData = localStorage["tasksList"];
+
+  if (taskData !== undefined) {
+    $scope.tasks = JSON.parse(taskData);
+  }
+
   $scope.searchEnter = function () {
     if (
       event.which === 13 &&
@@ -15,8 +21,9 @@ app.controller("app", ($scope) => {
 
   $scope.addTask = function () {
     $scope.tasks.push({ task: $scope.task, status: false });
-    console.log($scope.tasks);
     $scope.task = null;
+    localStorage["tasksList"] = JSON.stringify($scope.tasks);
+    console.log(localStorage["tasksList"]);
   };
 
   $scope.contentEdit = function () {
