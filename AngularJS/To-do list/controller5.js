39c87bb5873f4b9fb592b0ab1dda49afafb41627
matthew1996i <matthew199624@gmail.com>
2021-02-04 18:06:13 -0300

    complete the todo list

--- AngularJS/To-do list/controller5.js
+++ AngularJS/To-do list/controller5.js
@@ -15,24 +15,30 @@ app.controller("app", ($scope) => {
       $scope.task !== undefined &&
       $scope.task !== null
     ) {
-      $scope.addTask();
+      $scope.addTask({ taskMessage: $scope.task, status: false });
     }
   };
 
-  $scope.addTask = function () {
-    $scope.tasks.push({ task: $scope.task, status: false });
+  $scope.addTask = function (task) {
+    $scope.tasks.push(task);
     $scope.task = null;
     localStorage["tasksList"] = JSON.stringify($scope.tasks);
-    console.log(localStorage["tasksList"]);
   };
 
-  $scope.contentEdit = function () {
+  $scope.contentEdit = function (msg) {
+    for (i = 0; i < $scope.tasks.lenght; i++) {
+      if ($scope.tasks[i].taskMessage == msg) {
+        $scope.tasks[i].taskMessage = event.target.innerText;
+      }
+    }
+
+    event.target.contentEditable =
+      event.target.contentEditable === "false" ? "true" : "false";
+  };
+
+  $scope.enterAgain = function () {
     if (event.which === 13) {
-      event.target.contentEditable = "false";
-    } else {
-      event.target.contentEditable = !event.target.contentEditable
-        ? "false"
-        : "true";
+      $scope.contentEdit();
     }
   };
 });
